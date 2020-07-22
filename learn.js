784a975461521feec642fd35b5822b5142ff2042
upMyCode <alorine99@gmail.com>
2020-07-22 18:06:07 +0400

    firstKnowledge

---
+++ learn.js
@@ -0,0 +1,19 @@
+const myProg = {
+    myLang: "JavaScript"
+};
+
+console.log(myProg["myLang"]);
+
+const question = confirm('Вы классный прогер???');
+
+console.log(question);
+
+const secondQuestion = prompt('Вы парокрастинатор?', 'Дыа');
+
+console.log(typeof(secondQuestion));
+
+let rundom = 'viseman';
+console.log(`https://something/${rundom}/6`);
+
+
+console.log( 2 * 5 - 2 == '8');
