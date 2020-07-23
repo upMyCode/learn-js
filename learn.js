09fb808b0505bd8dbd5d70e7ceef28db6619a30e
upMyCode <alorine99@gmail.com>
2020-07-23 18:08:37 +0400

    add loop

--- learn.js
+++ learn.js
@@ -1,21 +1,42 @@
 // ****************lesson__1**************
 
-const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?','');
+// const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?','');
 
-const personalMovieDB = {
-    count : numberOfFilms,
-    movie: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count : numberOfFilms,
+//     movie: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const question = prompt(' Что вы смотрели в последний раз?', ''),
-      question2 = prompt('На сколько вы его оцените?', ''),
-      question3 = prompt(' Что вы смотрели в последний раз?', ''),
-      question4 = prompt('На сколько вы его оцените?', '');
+// const question = prompt(' Что вы смотрели в последний раз?', ''),
+//       question2 = prompt('На сколько вы его оцените?', ''),
+//       question3 = prompt(' Что вы смотрели в последний раз?', ''),
+//       question4 = prompt('На сколько вы его оцените?', '');
       
-personalMovieDB.movie[question] = question2;
-personalMovieDB.movie[question3] = question4;
+// personalMovieDB.movie[question] = question2;
+// personalMovieDB.movie[question3] = question4;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// // (a > 0) ? console.log('true') : console.log('false');
+
+// let a = 10;
+
+// switch(a){
+//     case 49:
+//         console.log('No');
+//         break;
+//     default:
+//         console.log('Что за  число такое?');
+//         break;
+// }
+
+
+let num = 100;
+
+while (num <= 97){
+    console.log('Привет');
+    num++;
+}
