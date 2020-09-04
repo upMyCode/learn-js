c848fae1e1416692fed8a12ebd59e5394a57f8dd
Ulad <alorine99@gmail.com>
2020-09-04 09:12:43 +0300

    thisContext

--- learnjs2.js
+++ learnjs2.js
@@ -1,7 +1,67 @@
 'use strict'
 
-function getThis(){
-    console.log(this);
+// function getThis(a, b){
+//     console.log(this);
+//         function abc(){
+//             console.log(this)
+//             return a + b;
+//         }
+//         console.log(abc())
+// }
+
+// getThis(5, 4);
+
+
+// const obj = {
+//     a : 20,
+//     b : 30,
+//     sum: function(){
+//             function sum(){
+//                 console.log(this);
+//             }
+//             sum();
+//     }
+// }
+
+// obj.sum();
+
+// function Constr(name, age){
+//     this.age = age;
+//     this.name = name;
+// }
+
+// let a = new Constr('Johan', 31);
+
+// console.log(a.age);
+// console.log(Constr);
+
+
+// function test(pc){
+//     console.log(this.name + pc);
+// }
+
+// const obj = {
+//     name : 'Johan'
+// };
+
+// // test.call(obj);
+// test.apply(obj, ['Max']);
+
+// test();
+// var a = 10;
+// var c = 11; 
+
+// while(a < 16){
+//     a = a+1;
+//     c = c+1;
+// }
+
+// console.log(a, c);
+
+function count(num){
+    console.log( num*this );
 }
 
-getThis();
+const dobl = count.bind(2);
+
+console.log(dobl(6));
